680d7d24ce64a81c01dae4c4c3c1963311c639e5
姬天戲 <ji1tian1xi4@gmail.com>
2017-03-17 22:17:31 +0800

    Update and rename 預測/MR/SY_YC1/類別_映射者.JAVA to Predict/MR/SY_PREDICT1/MyMapper.java

---
+++ Predict/MR/SY_PREDICT1/MyMapper.java
@@ -0,0 +1,25 @@
+package cgz.sy.cyf.predict1;
+
+import com.aliyun.odps.data
+import com.aliyun.odps.mapred
+import java.io
+
+public class MyMapper extends MapperBase
+{
+	private Record key, value;
+    
+	public void setup(TaskContext context) throws IOException
+	{
+		key = context.createMapOutputKeyRecord();
+		value = context.createMapOutputValueRecord();
+	}
+
+	public void map(long recordNum, Record record, TaskContext context) throws IOException
+	{
+		key.setBigint("item_id", record.getBigint("item_id"));
+		key.setBigint("hxitem_id", record.getBigint("hxitem_id"));
+		value.setDouble("hxitem_dpd", record.getDouble("hxitem_dpd"));
+		value.setBigint("hxitem_tcs", record.getBigint("hxitem_tcs"));
+		context.write(key, value);
+	}
+}
