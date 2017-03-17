4df7b72801f5b94d43b0aee8496ff75aef0dac0e
姬天戲 <ji1tian1xi4@gmail.com>
2017-03-17 22:24:25 +0800

    Update MyReducer.java

--- Predict/MR/SY_PREDICT1/MyReducer.java
+++ Predict/MR/SY_PREDICT1/MyReducer.java
@@ -1,55 +1,60 @@
 package cgz.sy.cyf.yc1;
 
-public class 類別_歸約者 extends com.aliyun.odps.mapred.ReducerBase
+import com.aliyun.odps.mapred;
+import com.aliyun.odps.data;
+import java.io;
+import java.util;
+
+public class MyMapper extends ReducerBase
 {	
-	private com.aliyun.odps.data.Record 結果;
+	private Record result;
 	
-	public void setup(TaskContext 上下文) throws java.io.IOException
+	public void setup(TaskContext context) throws IOException
 	{
-		結果 = 上下文.createOutputRecord();
+		result = context.createOutputRecord();
 	}
 	  
-	public void reduce(com.aliyun.odps.data.Record 鍵, java.util.Iterator<com.aliyun.odps.data.Record> 迭代_值, TaskContext 上下文) throws java.io.IOException
+	public void reduce(Record key, Iterator<Record> values, TaskContext context) throws IOException
 	{
-		結果.setBigint("item_id", 鍵.getBigint("item_id"));
-		java.util.Vector<類別_候選商品> 向量_候選商品 = new java.util.Vector<類別_候選商品>();
+		result.setBigint("item_id", key.getBigint("item_id"));
+		java.util.Vector<HxItem> hxitems = new Vector<HxItem>();
     	
-		while(迭代_值.hasNext())
+		while(values.hasNext())
 		{
-			com.aliyun.odps.data.Record 值 = 迭代_值.next();
-			long 候選商品標識 = 值.getBigint("hxitem_id");
-			double 候選商品搭配度 = 值.getDouble("hxitem_dpd");
-			long 候選商品套參數 = 值.getBigint("hxitem_tcs");
+			Record val = values.next();
+			long hxitem_id = val.getBigint("hxitem_id");
+			double hxitem_dpd = val.getDouble("hxitem_dpd");
+			long hxitem_tcs = val.getBigint("hxitem_tcs");
     		
-			向量_候選商品.add(new 類別_候選商品(候選商品標識, 候選商品搭配度, 候選商品套參數));
+			hxitems.add(new HxItem(hxitem_id, hxitem_dpd, hxitem_tcs));
 		}
     	
-		java.util.Collections.sort(向量_候選商品);
+		Collections.sort(hxitems);
     	
-		for(int 甲 = 0; 甲 < 向量_候選商品.size(); 甲++)
+		for(int i = 0; i < hxitems.size(); i++)
 		{
-			結果.setBigint("hxitem_id", 向量_候選商品.get(甲).標識);
-			結果.setDouble("hxitem_dpd", 向量_候選商品.get(甲).搭配度 * 向量_候選商品.get(甲).套參數);
-			上下文.write(結果);
+			result.setBigint("hxitem_id", hxitems.get(i).id);
+			result.setDouble("hxitem_dpd", hxitems.get(i).dpd * hxitems.get(i).tcs);
+			context.write(result);
 		}	
 	}
     
-	class 類別_候選商品 implements Comparable<類別_候選商品>
+	class HxItem implements Comparable<HxItem>
 	{
-		long 標識;
-		double 搭配度;
-		double 套參數;
+		long id;
+		double dpd;
+		double tcs;
 		
-		類別_候選商品(long 標, double 搭, long 套)
+		HxItem(long i, double d, long t)
 		{
-			標識 = 標;
-			搭配度 = 搭;
-			套參數 = 1 - Math.pow(5, -套);
+			id = i;
+			dpd = d;
+			tcs = 1 - Math.pow(5, -t);
 		}
 		
-		public int compareTo(類別_候選商品 另)
+		public int compareTo(HxItem a)
 		{
-			return -Double.compare(搭配度 * 套參數, 另.搭配度 * 另.套參數);
+			return -Double.compare(dpd * tcs, a.dpd * a.tcs);
 		}
 	}
 }
